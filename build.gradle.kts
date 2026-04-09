64cb4d61943af0f066e82771f4d6f8a0916e3c79
NaveenKrishna77 <naveenkrishna6643@gmail.com>
2026-04-09 15:16:46 -0400

    � first commit
    First commit
    Testing

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+import org.apache.tools.ant.Main
+import org.gradle.api.internal.plugins.MainClass
+
+plugins {
+    java
+    application
+}
+
+group = "com.naveen"
+version = "1.0-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+application {
+    mainClass.set("com.naveen.raytracer.Main")
+}
---
+++ src/main/java/com/naveen/raytracer/math/Vector3.java
@@ -0,0 +1,4 @@
+package com.naveen.raytracer.math;
+
+public class Vector3 {
+}
