84727f7f4b97d5251f5707f665d517dec81f24b3
Naveen <krishna.naveen1226@gmail.com>
2026-05-24 14:51:44 -0400

    Add method

--- src/main/java/com/naveen/raytracer/Main.java
+++ src/main/java/com/naveen/raytracer/Main.java
@@ -1,7 +1,12 @@
 package com.naveen.raytracer;
 
+import com.naveen.raytracer.math.Vector3;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Testing");
+        Vector3 vec1 = new Vector3(1, 2,3 );
+        Vector3 vec2 = new Vector3(3, 4, 5);
+        Vector3 vec3 = vec1.add(vec2);
+        System.out.println(vec3.getX()+" " +vec3.getY()+" " +vec3.getZ());
     }
 }
--- src/main/java/com/naveen/raytracer/math/Vector3.java
+++ src/main/java/com/naveen/raytracer/math/Vector3.java
@@ -1,4 +1,50 @@
 package com.naveen.raytracer.math;
 
 public class Vector3 {
+
+    private double x;
+    private double y;
+    private double z;
+
+    public Vector3(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
+
+
+    public Vector3 add(Vector3 v) {
+        Vector3 result = new Vector3(this.x, this.y, this.z);
+        result.setX(this.x+v.getX());
+        result.setY(this.y+v.getY());
+        result.setZ(this.z+v.getZ());
+
+        return result;
+    }
+
+
 }
